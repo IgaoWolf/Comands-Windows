d3aca2f292caebda5c35e977feca9ebc9284ec33
Igor Augusto Wolf <contatowolfcall@outlook.com>
2024-03-07 17:06:45 -0300

    Create Gerenciamento.sh

---
+++ Gerenciamento.sh
@@ -0,0 +1,12 @@
+COMANDO WINDOWS POWERSHEL 
+
+
+Comando para ver o hostname da máquina
+hostname
+
+Reiniciar servidor windows via linha de comando
+Restart-Computer -ComputerName "nome da máquina" -Force
+
+Reiniciar servidor windows via linha de comando
+New-Item -Path "C:\Users\Administrator\Ubuntu -ItemType Directory
+
