9c9335869fa9080e766f27a5288b72f59046dfc4
Igor Augusto Wolf <81160709+IgaoWolf@users.noreply.github.com>
2024-03-07 17:18:43 -0300

    Update Gerenciamento.sh

--- Gerenciamento.sh
+++ Gerenciamento.sh
@@ -2,16 +2,10 @@ COMANDO WINDOWS POWERSHEL
 
 Comando para ver o hostname da máquina
 
-```sh
-hostname
-```
+`hostname`
 
 Reiniciar servidor windows via linha de comando
-```sh
-Restart-Computer -ComputerName "nome da máquina" -Force
-```
+`Restart-Computer -ComputerName "nome da máquina" -Force`
 
 Criar um novo Diretorio
-```sh
-New-Item -Path "C:\Users\Administrator\Ubuntu -ItemType Directory
-```
+`New-Item -Path "C:\Users\Administrator\Ubuntu -ItemType Directory`
